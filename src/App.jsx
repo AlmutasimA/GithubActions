7db19e27ea9055fb97a856845d89015a55b4f978
Peter Faso <quinlayen@gmail.com>
2025-03-12 13:10:32 +0300

    deploying code

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
   return (
     <>
       <header>
-        <div id="logo-img">
-          <img src={logo} />
-        </div>
-        <h1>Learn & Master GitHub Actions</h1>
+      <div id="logo-img">
+      <img src={logo} />
+      </div>
+      <h1>
+      Learn & Master GitHub Actions
+      </h1>
       </header>
+
       <MainContent />
     </>
   );
